3982224e3f733cfac2e7b0ed3d9915e0dd1ae8e0
strongwarmi <elianasc10@hotmail.com>
2017-04-17 20:00:37 -0500

    ekis funciona de forma individal, solo para la primera imagen

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',function(){
+var fotospandas=document.getElementsByClassName("ContenedorFoto");
+console.log(fotospandas);
+
+});
+ekis1.addEventListener("click",function(){
+  document.getElementById("panda1").style.display="none";
+});
